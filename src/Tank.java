652488557bec02a66170fc901ef1934b177c970a
JhonStar <JhonStar113@gmail.com>
2021-12-28 13:59:00 +0300

    6com

--- src/Tank.java
+++ src/Tank.java
@@ -26,6 +26,7 @@ public class Tank {
             System.out.println("1");
             System.out.println("2");
             System.out.println("3");
+
             System.out.println("4");
             i = -fuel;
         }
